d7ace2e826a7810f503b34b319d3855bd20ffd98
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-11-05 16:57:31 +0100

    feat: lampadina

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log ('ciao mondo')
+
+
+const lampadina = document.getElementById('lampadina');
+const button = document.getElementById('btnAccendi');
+
+button.addEventListener('click', function() {
+    lampadina.src = "/img/yellow_lamp.png";
+});
